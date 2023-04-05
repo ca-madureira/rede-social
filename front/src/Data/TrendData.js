9794e0f4c21ec9bc08486a72453b4d8ef6fdccdd
camila-madureira <121184472+ca-madureira@users.noreply.github.com>
2023-04-05 16:52:16 -0300

    add front files

---
+++ front/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+  {
+    name: 'Nodejs',
+    shares: 97,
+  },
+  {
+    name: 'Javascript',
+    shares: 80.5,
+  },
+  {
+    name: 'Python',
+    shares: 75.5,
+  },
+  {
+    name: 'Reactjs',
+    shares: 72,
+  },
+  {
+    name: 'Java',
+    shares: 71.9,
+  },
+  {
+    name: 'Nextjs',
+    shares: 52.6,
+  },
+];
